bb855af064616314345183bff54a951017311c4e
Shweta Tyagi <123883560+ShwetaTyagi1@users.noreply.github.com>
2024-09-30 02:47:25 +0530

    Update Color-To-Gray.py

--- Color-To-Gray.py
+++ Color-To-Gray.py
@@ -1,17 +1,5 @@
 
 import cv2 #openCV used as cv2 in python
-"""
-img1=cv2.imread("C:\\Users\\Shweta Tyagi\\OneDrive\\Pictures\\user.jpg") #imread function is used to take input in form of an image from any location on the system
-#using \\ or / incase of \ because of unicode error
-#imread takes two parameter-location, 0/1/-1 0-returns a grayscale img, 1 returns colourful image , -1-unchanged ie same as original image but just with a litle higher saturation , default = 1  
-img1=cv2.resize(img1, (800,800)) #(width,height)  resized image so it opens of a particular size in the new window 
-print(img1)
-cv2.imshow("original", img1) #original is the name i gave to the new window in which the image img1 will open
-cv2.waitKey() #default arg = 0 ie indefinite static window, else we can give number of milliseconds
-cv2.destroyAllWindows() #after code terminates, all windows will be destroyed
-"""
-
-# Project: Image conversion to grayscale
 
 # Took input from user
 path = input("Enter path of the image and its name: ")
